3e2bd28a8c7d16650e805799c03403bed424ddaa
Wilmer <sique.wilmer94@gmail.com>
2019-05-20 15:38:54 -0600

    Primer commit

---
+++ precache-manifest.79a885e0f8abf1cf84d2009b2a306d30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e5df429ed51fa38bf0197f7450f9ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70e55b744976ff2d7f88",
+    "url": "/static/css/main.3a47da37.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "70e55b744976ff2d7f88",
+    "url": "/static/js/main.9d0f0657.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
